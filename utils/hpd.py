9c3ac1004799cfbf1d105b94ddb8480ae4cb6fc7
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2026-09-05 23:22:35 +0300

    utils/hpd: Follow the opened card instead of hard-coding card0
    Filter the udev events by the card that was actually opened, use
    Connector.refresh() so the connection status is updated too, and
    print it.

--- utils/hpd.py
+++ utils/hpd.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import pprint
 
 import pyudev
@@ -7,19 +8,23 @@ import pyudev
 import kms
 
 card = kms.Card()
-connectors = card.connectors
+card_name = os.path.basename(card.dev_path)
 
 context = pyudev.Context()
-
-dev = pyudev.Devices.from_name(context, 'drm', 'card0')
 
 monitor = pyudev.Monitor.from_netlink(context)
 monitor.filter_by('drm')
 
 for device in iter(monitor.poll, None):
-    if 'HOTPLUG' in device:
-        print('== HPD ==')
-        for conn in connectors:
-            conn.refresh_modes()
-            strs = (conn.fullname, ['{}x{}'.format(m.hdisplay, m.vdisplay) for m in conn.modes])
-            pprint.pprint(strs, compact=True, width=120)
+    if device.sys_name != card_name or 'HOTPLUG' not in device:
+        continue
+
+    print('== HPD ==')
+    for conn in card.connectors:
+        conn.refresh()
+        strs = (
+            conn.fullname,
+            'connected' if conn.connected else 'disconnected',
+            [f'{m.hdisplay}x{m.vdisplay}' for m in conn.modes],
+        )
+        pprint.pprint(strs, compact=True, width=120)
