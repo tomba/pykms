cbcc08a866cad9fa714d2ca1b4ef82d80843fc82
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2026-09-05 23:17:02 +0300

    helpers: Re-export the fourcc helpers from pixutils
    pixutils.formats has identical fourcc_to_str() and str_to_fourcc()
    implementations.

--- kms/helpers.py
+++ kms/helpers.py
@@ -1,18 +1,5 @@
 from __future__ import annotations
 
+from pixutils.formats import fourcc_to_str, str_to_fourcc
+
 __all__ = ['fourcc_to_str', 'str_to_fourcc']
-
-
-def fourcc_to_str(fourcc: int):
-    return ''.join(
-        (
-            chr((fourcc >> 0) & 0xFF),
-            chr((fourcc >> 8) & 0xFF),
-            chr((fourcc >> 16) & 0xFF),
-            chr((fourcc >> 24) & 0xFF),
-        )
-    )
-
-
-def str_to_fourcc(s: str):
-    return ord(s[0]) << 0 | ord(s[1]) << 8 | ord(s[2]) << 16 | ord(s[3]) << 24
